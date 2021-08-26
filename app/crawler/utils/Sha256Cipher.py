d2236f937183e88ecffa43f978c2ddd6c6e43702
Rakuzan07 <deuconicas@gmail.com>
2021-08-26 12:43:50 +0200

    Aggiunto Chiper per password

---
+++ app/crawler/utils/Sha256Cipher.py
@@ -0,0 +1,17 @@
+import hashlib
+
+
+class SHA256Cipher(object):
+
+    def __init__(self, value : str):
+        self.value = value
+        self.encrypted = None
+
+    def encrypt(self):
+      self.encrypted = hashlib.sha256(self.value).hexdigest()
+
+    def get_encrypted_value(self):
+        return self.encrypted
+
+    def __eq__(self, other):
+        return self.value==other
