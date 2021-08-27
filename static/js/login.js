e183a009666ed135a8560a075631b416f6a02649
Lorenzo <46032643+LeRoy19@users.noreply.github.com>
2021-08-27 11:16:34 +0200

    fixed login bugs

--- static/js/login.js
+++ static/js/login.js
@@ -2,7 +2,6 @@ function spotifyLogin() {
     const authEndpoint = 'https://accounts.spotify.com/authorize';
     const clientId = '7dfcff2789584927a64d6685f9f0d614';
     const redirectUri = encodeURIComponent('http://localhost:8000/home');
-    console.log(redirectUri);
     const scopes = ['user-read-recently-played'];
 
     window.location = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&show_dialog=true`;
